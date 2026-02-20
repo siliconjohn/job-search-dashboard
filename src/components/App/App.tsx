52631a54101d6e339ab5b8cbfb49738df4a596fd
John Doerfler <j5602396@protonmail.com>
2026-02-20 11:26:24 -0600

    cleanup refactor

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,71 +1,57 @@
-import { Flex, Layout, Button,  ConfigProvider, theme } from 'antd';
-import ActivityLog from '../ActivityLog/ActivityLog';
 import { useContext } from 'react';
+import { Flex, Layout, Button, ConfigProvider, theme } from 'antd';
 import { ThemeContext } from '../../contexts/ThemeContext';
 import ThemeProvider from '../../contexts/ThemeProvider';
+import ActivityLog from '../ActivityLog/ActivityLog';
 const { Header, Content } = Layout;
 import './App.css';
 
-const layoutStyle: React.CSSProperties = {
-    width: '100vw',
-    height: '100vh',
-};
-
-const headerStyle: React.CSSProperties = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    color: '#fff'
-}
-
-const contentStyle: React.CSSProperties = {
-    padding: '24px',
-    overflow: 'scroll',
-};
-
 const AppLayout = () => {
-    const { themeName, toggleTheme  } = useContext(ThemeContext)!;
+    const { themeName, toggleTheme } = useContext(ThemeContext)!;
 
-    return ( 
+    return (
         <ConfigProvider
             theme={{
-                algorithm: theme[themeName],      
+                algorithm: theme[themeName],
                 token: {
-                    colorPrimary: '#52c41a',         
-                    colorSuccess: '#2d6d0d',        
-                    colorInfo: '#1677ff',           
+                    colorPrimary: '#52c41a',
+                    colorSuccess: '#2d6d0d',
+                    colorInfo: '#1677ff',
                     colorWarning: '#faad14',
                     colorError: '#ff4d4f',
-                    colorPrimaryHover: '#73d13d',   
-                    colorPrimaryActive: '#389e0d',  
+                    colorPrimaryHover: '#73d13d',
+                    colorPrimaryActive: '#389e0d',
                     borderRadius: 6,
-                }
+                },
             }}
         >
             <Flex gap="middle" wrap>
-                <Layout style={layoutStyle}>
-                    <Header style={headerStyle}>
+                <Layout style={{ width: '100vw', height: '100vh' }}>
+                    <Header
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            color: '#fff',
+                        }}
+                    >
                         <h1>Job Search Dashboard</h1>
-                        <Button 
-                            size="small"
-                            type="primary"
-                            onClick={ toggleTheme}
-                        >
+                        <Button size="small" type="primary" onClick={toggleTheme}>
                             Toggle Theme
                         </Button>
                     </Header>
-                    <Content style={contentStyle}>
-                        <ActivityLog/>
+                    <Content style={{ padding: '24px', overflow: 'scroll' }}>
+                        <ActivityLog />
                     </Content>
                 </Layout>
             </Flex>
         </ConfigProvider>
-    )
+    );
 };
 
 const App: React.FC = () => (
-    <ThemeProvider> 
-        <AppLayout/>
+    <ThemeProvider>
+        <AppLayout />
     </ThemeProvider>
 );
 
