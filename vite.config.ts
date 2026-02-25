f61e374d14739ef3868dbd6de8aea8b86b040c90
John Doerfler <j5602396@protonmail.com>
2026-02-25 10:49:46 -0600

    fixed build types error

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-/// <reference types="vitest" />     // ← Add this line (or "vitest/config" in some versions)
+/// <reference types="vitest/config" />
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
